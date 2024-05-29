34dd82e6d8b7a8f49e02a3f8ff379d58c8acaf46
Carl Moore <carlmoore256@gmail.com>
2024-05-29 13:00:45 -0700

    Initial Commit

---
+++ ARVirtualCamStreamer/ARVirtualCamStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARVirtualCamStreamerApp.swift
+//  ARVirtualCamStreamer
+//
+//  Created by Carl Moore on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ARVirtualCamStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
